975f5e9d44fcb77a9053ed7c88d9651254230fb7
leipero <lei.pero@gmail.com>
2022-02-14 06:44:49 +0100

    simplify build.sh

--- build.sh
+++ build.sh
@@ -13,13 +13,13 @@ sed -i -e '/^_FS_bypass_compositor=/c\_FS_bypass_compositor="true"' ${CFG}
 sed -i -e '/^_win10_default=/c\_win10_default="true"' ${CFG}
 sed -i -e '/^_staging_pulse_disable=/c\_staging_pulse_disable="true"' ${ADVCFG}
 
-function setenvnofshack() {
-  sed -i -e '/^_proton_fs_hack=/c\_proton_fs_hack="false"' ${CFG}
-}
-
 function setenvfshack() {
   sed -i -e '/^_proton_fs_hack=/c\_proton_fs_hack="true"' ${CFG}
   sed -i -e '/^_community_patches=/c\_community_patches="amd_fsr_fshack.mypatch"' ${CFG}
+}
+
+function setenvnofshack() {
+  sed -i -e '/^_proton_fs_hack=/c\_proton_fs_hack="false"' ${CFG}
 }
 
 function pack() {
@@ -42,7 +42,7 @@ function pack() {
   mv $winever.tar.xz ../../../../..
 }
 
-read -p "Choose staging version (Default: latest release, empty for git version): " -e -i 'v7.2' stgver
+read -rp 'Choose staging version (e.g. "7cfceb7" or "v6.0", leave empty for git): ' -e stgver
 if [ -z "$stgver" ]; then
   echo "Using git version."
 else
@@ -50,19 +50,11 @@ else
   echo "Using ${stgver}."
 fi
 
-read -p "1. No fshack.
-2. Fshack + AMD FSR.
-Choose build (1-2): " -e buildenv
- case $buildenv in
-  1)
-    setenvnofshack
-    ;;
-  2)
-    setenvfshack
-    ;;
-  *)
-    echo "Invalid Input"
-    ;;
-esac
+read -rp "Use Valve fshack patch?"$'\n> N/y : ' buildenv
+if [[ "$buildenv" =~ [yY] ]]; then
+  setenvfshack
+else
+  setenvnofshack
+fi
 
 makepkg -s && pack
